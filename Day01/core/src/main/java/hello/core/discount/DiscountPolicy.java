746d983f9669ba57a407bca3adbb94fdba9421ff
Pham Thi Que Nhu <110645380+phamquenhu95@users.noreply.github.com>
2023-04-25 18:28:40 +0900

    Delete Day01/core directory

--- Day01/core/src/main/java/hello/core/discount/DiscountPolicy.java
+++
@@ -1,7 +0,0 @@
-package hello.core.discount;
-
-import hello.core.member.Member;
-
-public interface DiscountPolicy {
-  int discountAmount(Member member, int price);
-}
--- Day01/core/src/main/java/hello/core/order/OrderService.java
+++
@@ -1,8 +0,0 @@
-package hello.core.order;
-
-import hello.core.member.Member;
-
-public interface OrderService {
-  Order createOrder(Long memberId, String itemName, int itemPrice);
-
-}
